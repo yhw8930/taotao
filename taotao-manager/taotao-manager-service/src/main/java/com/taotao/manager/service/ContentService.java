de3d0697d994cafa32dab48e76837aadca8016bd
yhw8930 <1170930576@qq.com>
2018-10-22 22:31:42 +0800

    q

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/manager/service/ContentService.java
@@ -0,0 +1,8 @@
+package com.taotao.manager.service;
+
+import com.taotao.manager.pojo.Content;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContentService extends BaseService<Content>{
+}
