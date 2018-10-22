39d6187ab32338aa3a5045206085a8946d558f35
yhw8930 <1170930576@qq.com>
2018-10-22 22:32:03 +0800

    q

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/manager/service/ContentCategoryService.java
@@ -0,0 +1,8 @@
+package com.taotao.manager.service;
+
+import com.taotao.manager.pojo.ContentCategory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContentCategoryService extends BaseService<ContentCategory>{
+}
